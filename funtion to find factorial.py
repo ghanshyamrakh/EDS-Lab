b1d0f7a7d0829319e120d2d17ac32a49dedfc405
ghanshyamrakh <ghanshyamrakh@gmail.com>
2026-05-06 12:04:08 +0530

    Delete funtion to find factorial.py

--- funtion to find factorial.py
+++
@@ -1,9 +0,0 @@
-def factorial(n):
-    if n < 0:
-        return "Factorial not defined for negative numbers"
-    elif n == 0 or n == 1:
-        return 1
-    else:
-        return n * factorial(n - 1)
-num = int(input("Enter an integer: "))
-print("Factorial is:", factorial(num))
